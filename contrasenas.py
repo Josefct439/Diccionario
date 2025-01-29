21f928a2fb7e490eec581765d9567709fd2a190b
José Cabarcas <132719140+Josefct439@users.noreply.github.com>
2025-01-28 20:19:10 -0500

    Update contrasenas.py

--- contrasenas.py
+++ contrasenas.py
@@ -1,8 +1,1 @@
-import random
-characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-password_length = int(input("Introduce la longitud de la contraseña que deseas: "))
-generated_password = ""
-for _ in range(password_length):
-    generated_password += random.choice(characters)
-def print_password(password):
-    print(f"Tu contraseña generada es: {password}")
+.
