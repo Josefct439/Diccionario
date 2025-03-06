37d7d7948f36b128c131757276d2c7f25f9b73b2
José Cabarcas <132719140+Josefct439@users.noreply.github.com>
2025-03-06 13:04:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-meme_dict = {
-            "CRINGE": "Algo excepcionalmente raro o embarazoso",
-            "LOL": "Una respuesta común a algo gracioso",
-            "ROFL" : "una respuesta a una broma",
-            }
-
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
-
-if word in meme_dict.keys():
-    # ¿Qué debemos hacer si se encuentra la palabra?
-    print(word,"significa:",meme_dict[word])
-else:
-    # ¿Qué hacer si no se encuentra la palabra?
-    print("Esa palabra no esta en el diccionario")
+a
